17a2fd4f90edda41d794787f29405a2ad8f86c42
Claudiney <claudineydelima@gmail.com>
2020-04-04 18:30:42 -0300

    Cálculadora concluida

--- Calculadora.java
+++ Calculadora.java
@@ -1,11 +1,10 @@
-
 public class Main
 {
 	public static void main(String[] args) {
 	    
 		System.out.println(Soma(2,4));
 		System.out.println(Multiplica(2,4));
-	}
+	}	
 	
 	public static int Soma(int a, int b){
 	    return a + b;
