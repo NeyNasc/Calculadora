87195bcea43e3e96794d163dd81b0eff4a32821d
Claudiney <claudineydelima@gmail.com>
2020-04-04 18:28:29 -0300

    Adicionando função de soma

---
+++ Calculadora.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		System.out.println(Soma(2,4));
+	}
+	
+	public static int Soma(int a, int b){
+	    return a + b;
+	    }
+}
